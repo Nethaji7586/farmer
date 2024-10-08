6f3019e47d20765566c122cfe728e44ffa04e5bb
Nethaji.v <155124384+Nethaji7586@users.noreply.github.com>
2024-10-08 23:27:07 +0530

    Update2 urls.py

--- farming/urls.py
+++ farming/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import weather_forecast
 
 urlpatterns = [
-    path('weather/', weather_forecast, name='weather_forecast'),
+    path('', weather_forecast, name='weather_forecast'),
 ]
